5f63c5770ad64efb396f487997f2cbfcd7e4d8fb
Nico Rithner <nicorithner@gmail.com>
2022-04-04 16:10:32 -0600

    Complete lesson 7 - variables

--- testProject/main.lua
+++ testProject/main.lua
@@ -1,4 +1,14 @@
+-- message = "Lua is awesome!!!"
+local message = 5 -- we can also print numbers
+local chicken = 10
+local output = chicken + 1 -- prints 11
+message = message - 1
+-- local chicken = 'math is fun'
+-- local output = chicken * 1 -- errors out becuase we are trying to perform math on a string
+
+
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print("Hello World")
+  -- love.graphics.print(output)
+  love.graphics.print(message)
 end
