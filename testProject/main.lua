2e746270ce3595275dc024408f5033d8a5c2f38e
Nico Rithner <nicorithner@gmail.com>
2022-04-06 11:54:31 -0600

    Complete lesson 10 - While loops

--- testProject/main.lua
+++ testProject/main.lua
@@ -16,8 +16,8 @@
 
 -- *----- Conditionals --------*
 -- *--- If statements ---*
-local message = 0
-local condition = 25
+-- local message = 0
+-- local condition = 25
 
 -- if condition > 0 then
 --   message = 1
@@ -37,15 +37,35 @@ local condition = 25
 
 -- *--- If - Elseif statements ---*
 
-if condition > 0 then
-  message = 1
-elseif condition < -10 then
-  message = -1
-else
-  message = "no conditions met"
+-- if condition > 0 then
+--   message = 1
+-- elseif condition < -10 then
+--   message = -1
+-- else
+--   message = "no conditions met"
+-- end
+
+-- function love.draw()
+--   love.graphics.setFont(love.graphics.newFont(50))
+--   love.graphics.print(message)
+-- end
+
+-- *----- While Loops --------*
+local message = 0
+local test = 0
+
+-- while message < 10 do
+--   message = message + 1
+-- end
+
+while message < 100 do
+  message = message + 1
+  test = test - 5
 end
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print(message)
+  love.graphics.print(message, 0, 0)
+  love.graphics.print(test, 0, 60)
 end
+
