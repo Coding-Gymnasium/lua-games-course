bc3d8f65b3901dd3ce1920221f25969b9e35208e
Nico Rithner <nicorithner@gmail.com>
2022-04-06 11:17:18 -0600

    Complete lesson 8 - if statements

--- testProject/main.lua
+++ testProject/main.lua
@@ -1,14 +1,33 @@
+-- *----- Variables --------*
 -- message = "Lua is awesome!!!"
-local message = 5 -- we can also print numbers
-local chicken = 10
-local output = chicken + 1 -- prints 11
-message = message - 1
--- local chicken = 'math is fun'
--- local output = chicken * 1 -- errors out becuase we are trying to perform math on a string
+-- local message = 5 -- we can also print numbers
+-- local chicken = 10
+-- local output = chicken + 1 -- prints 11
+-- message = message - 1
+-- -- local chicken = 'math is fun'
+-- -- local output = chicken * 1 -- errors out becuase we are trying to perform math on a string
+--
+--
+-- function love.draw()
+--   love.graphics.setFont(love.graphics.newFont(50))
+--   -- love.graphics.print(output)
+--   love.graphics.print(message)
+-- end
+
+-- *----- Conditionals --------*
+local message = 0
+local condition = 25
+
+if condition > 0 then
+  message = 1
+end
+
+if condition <= 0 then
+  message = -1
+end
 
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  -- love.graphics.print(output)
   love.graphics.print(message)
 end
