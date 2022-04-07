a0db0886341240e9c891e1e974e6691ccf9402c3
Nico Rithner <nicorithner@gmail.com>
2022-04-07 11:16:21 -0600

    Complete lesson 22

--- shooting-gallery/main.lua
+++ shooting-gallery/main.lua
@@ -1,15 +1,18 @@
 function love.load()
+  target = {}
+  target.x = 300
+  target.y = 300
+  target.radius = 50
+  score = 0
+  timer = 0
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(0,1,0) -- green
-  love.graphics.rectangle("fill", 200, 400, 200, 100)
-
-  love.graphics.setColor(1,0.63,0) -- lighter orange
-  love.graphics.circle("fill", 300, 200, 100)
+  love.graphics.setColor(1,0,0)
+  love.graphics.circle("fill", target.x, target.y, target.radius)
 end
 
 --[[
@@ -37,7 +40,7 @@ end
   love.graphics.rectangle(mode, x, y, width, height)
   mode can be 'fill' or 'line'
   the x, y position of a rectangle is its top left corner.
-  
+
   Circle:
   the x, y position of a circle is its center.
   love.graphics.circle("fill", 0, 0, 100) -- would be a semi-circle on the top left corner
