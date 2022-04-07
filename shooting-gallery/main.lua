f970d98fe1339a18ff7cbf3767df39678d4f9849
Nico Rithner <nicorithner@gmail.com>
2022-04-07 11:26:10 -0600

    Add mouse button interaction

--- shooting-gallery/main.lua
+++ shooting-gallery/main.lua
@@ -5,6 +5,7 @@ function love.load()
   target.radius = 50
   score = 0
   timer = 0
+  gameFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -13,10 +14,24 @@ end
 function love.draw()
   love.graphics.setColor(1,0,0)
   love.graphics.circle("fill", target.x, target.y, target.radius)
+
+  love.graphics.setColor(1,1,1)
+  love.graphics.setFont(gameFont)
+  love.graphics.print(score, 0,0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  if button == 1 then
+    score = score + 1 
+  end
 end
 
 --[[
   Notes:
+  Mouse interaction:
+  love.mousepressed(x, y, button, istouch, presses)
+  button values: 1 equals left mouse button, 2 the secondary button.
+
   Order:
   Love will display what we draw first at the bottom and the next thing on top.
   For example, if the rectangle is drawn first - appears first in the code -
