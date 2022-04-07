78b85391a592f19960fd22957a990d77e33a2ce5
Nico Rithner <nicorithner@gmail.com>
2022-04-07 14:32:31 -0600

    Lesson 27. Add sprites

--- shooting-gallery/main.lua
+++ shooting-gallery/main.lua
@@ -6,6 +6,12 @@ function love.load()
   score = 0
   timer = 10
   gameFont = love.graphics.newFont(40)
+
+  sprites = {}
+  sprites.sky = love.graphics.newImage('assets/sky.png')
+  sprites.target = love.graphics.newImage('assets/target.png')
+  sprites.crosshairs = love.graphics.newImage('assets/crosshairs.png')
+  love.mouse.setVisible(false)
 end
 
 function love.update(dt)
@@ -19,13 +25,14 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(1,0,0)
-  love.graphics.circle("fill", target.x, target.y, target.radius)
-
+  love.graphics.draw(sprites.sky, 0, 0)
   love.graphics.setColor(1,1,1)
   love.graphics.setFont(gameFont)
   love.graphics.print(score, 0,0)
   love.graphics.print(math.ceil(timer), 300,0)
+
+  love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+  love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
@@ -43,10 +50,13 @@ function distanceBetween(x1, y1, x2, y2)
   return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2)
 end
 
+  -- *-------- Notes: ---------*
 --[[
-  Notes:
+  Drawing an image (png):
+  An image x,y is the top left corner of the image
+
   Calculating distance:
-  distance formula: 
+  distance formula:
     d = √(x2 - x1)² + (y₂ - y₁)²
   written in code:
     return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2)
