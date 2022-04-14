3fc4e6ea7646a936e1f0bac056abb5d19659bff2
Nico Rithner <nicorithner@gmail.com>
2022-04-14 17:12:33 -0600

    Add rotation about the center

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -6,27 +6,31 @@ function love.load()
   sprites.zombie = love.graphics.newImage('assets/sprites/zombie.png')
 
   player = {}
-  player.x = love.graphics.getWidth()/2
-  player.y = love.graphics.getHeight()/2
+  player.x = love.graphics.getWidth() / 2
+  player.y = love.graphics.getHeight() / 2
   player.speed = 180
+
+  tempRotation = 0
 end
 
 function love.update(dt)
   if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed*dt 
+    player.x = player.x + player.speed * dt
   end
   if love.keyboard.isDown("a") then
-    player.x = player.x - player.speed*dt 
+    player.x = player.x - player.speed * dt
   end
   if love.keyboard.isDown("s") then
-    player.y = player.y + player.speed*dt 
+    player.y = player.y + player.speed * dt
   end
   if love.keyboard.isDown("w") then
-    player.y = player.y - player.speed*dt 
+    player.y = player.y - player.speed * dt
   end
+
+  tempRotation = tempRotation + 0.01
 end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.player, player.x, player.y)
+  love.graphics.draw(sprites.player, player.x, player.y, tempRotation, nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
 end
