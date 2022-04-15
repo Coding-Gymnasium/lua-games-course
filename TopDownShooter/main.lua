26256e5ff3e844d9140283876289cd063c96108c
Nico Rithner <nicorithner@gmail.com>
2022-04-14 19:18:15 -0600

    Add player rotation follows the mouse

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -32,5 +32,10 @@ end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.player, player.x, player.y, tempRotation, nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
+
+  love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
+end
+
+function playerMouseAngle()
+  return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
 end
