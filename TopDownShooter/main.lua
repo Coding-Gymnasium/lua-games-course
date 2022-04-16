1c79080be7b1fa3d31919003c74f17eee402bc0e
Nico Rithner <nicorithner@gmail.com>
2022-04-16 13:00:35 -0600

    Add zombies rotation

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -34,7 +34,7 @@ function love.draw()
   love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
 
   for i,z in ipairs(zombies) do
-    love.graphics.draw(sprites.zombie, z.x, z.y)
+    love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerMouseAngle(z), nil, nil, sprites.zombie:getWidth() / 2, sprites.zombie:getHeight() / 2)
   end
 end
 
@@ -46,6 +46,10 @@ end
 
 function playerMouseAngle()
   return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function zombiePlayerMouseAngle(enemy)
+  return math.atan2(player.y - enemy.y, player.x - enemy.x)
 end
 
 function spawnZombie()
